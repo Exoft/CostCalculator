15192d3e782b15b6c9f011af7bc0b9d7e3253985
adroban <adroban@exoft.net>
2017-09-28 12:07:46 +0300

    all pages

--- calc.js
+++ calc.js
@@ -1,67 +1,54 @@
+var first = {name:'first', get}
+var os = {name:'os', android:24, ios:35, both:70};
+var screen = {name:'screen', sm:24, md:55, bg:77, lg:100};
+var design = {name:'design', stock:50, custom:150, animated:200};
+var api = {name:'api', with:300, simple:500, advanced:700};
+var aa = {name:'aa', email:150, soc:200, twoFactor:400};
+var ff = {name:'ff', dash:100, activ:150, userProf:200, textSearch:250, fileUp:300, multiSup:500};
+var dates = {name:'dates', geo:150, map:200, booking:250, calendar:300};
+var social = {name:'social', masseg:50, socShare:75, forums:150, push:200, rating:100}
+var ecom = {name:'ecom', payment:300, shopping:350, marketplace:300};
+var media = {name:'media', video:100, audio:100, photo:50};
+var admin = {name:'admin', cms:300, userAdmin:350, monitoring:400, payAdmin:450};
+var security = {name:'security', role:300, data:250, privecy:300};
+
+var arr = [first, os, screen, design, api, aa, ff, dates, social, ecom, media, admin, security, estimate];
 
 var price = 0;
-
-var os = {android:24, ios:35, both:70, page:1}
-
-var screen = {sm:24, md:55, bg:77, lg:100, page:2}
-
-var pages = ['first', 'os', 'screen', 'design', 'api', 'aa', 'ff', 'dates', 'social', 'ecom', 'media', 'admin', 'security', 'estimate'];
-
-
 var status;
 
-function nextStep(i) {
+// document.getElementById('prev').addEventListener('click', function(){
+// 	document.getElementById(pages[status]).style.display = "none";
+// 	document.getElementById(pages[status-1]).style.display = "table-cell";
 
-	var now = document.getElementById(pages[i]);
-	var next = document.getElementById(pages[i+1]);
+// 	status -= 1;
+// 	if (status<2) {
+// 		document.getElementById('prev').style.visibility = "hidden";
+// 	}
+// });
 
-	now.style.display = "none";
-	next.style.display = "table-cell";
+document.querySelector('main').addEventListener('click', function () {
+	var elem = event.target;
+	var par = elem.parentNode.id;
 
-	return status=i+1;
-};
+ 	for (var x = 0; x < arr.length; x++) {
+ 		if (par==arr[x].name) {
+			var a = Object.keys(arr[x]);
 
-function changePrice(n) {
-    price += n;
-	document.getElementById('price').innerHTML = "$" + price;
-};
+			for (var i = 0; i < Object.keys(arr[x]).length; i++) {
+	 			if (elem.id==a[i]) {
+	 				var now = document.getElementById(arr[x].name);
+					var next = document.getElementById(arr[x+1].name);
 
-document.getElementById('prev').addEventListener('click', function(){
-	document.getElementById(pages[status]).style.display = "none";
-	document.getElementById(pages[status-1]).style.display = "table-cell";
+					now.style.display = "none";
+					next.style.display = "table-cell";
 
-	status -= 1;
-	if (status<2) {
-		document.getElementById('prev').style.visibility = "hidden";
+					// return status=i+1;
+	 				break;
+	 			};
+			}
+		}
 	}
 });
 
-
-document.getElementById('get').addEventListener('click', function() {
-	nextStep(0)
-	changePrice(0);
-	document.getElementById('cost').style.visibility = "visible";
-});
-
-
-document.querySelector('#os').addEventListener('click', function () {
-	var elem = event.target.id;
-	var a = Object.keys(os);
-	for (var i = 0; i < Object.keys(os).length; i++) {
-	 	if (elem==a[i]) {
-	 		nextStep(os.page);
-
-	 	};
-}
-});
-
-document.querySelector('#screen').addEventListener('click', function () {
-	var elem = event.target.id;
-	var a = Object.keys(screen);
-	for (var i = 0; i < Object.keys(screen).length; i++) {
-	 	if (elem==a[i]) {
-	 		nextStep(screen.page);
-	 	};
-}
-});
 
